022d7eb978efd6491aff564831637726cef064c1
Shaf8808 <shadmankhan08@gmail.com>
2024-08-07 23:31:05 +0100

    Update .gitignore to ignore unnecessary files

--- interview-app/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/VideoInterview":["static\u002Fchunks\u002Fpages\u002FVideoInterview.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FVideoInterview","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
