b315dfc6549b7aa97c6686f4fd1e03d055a9f100
sammwas <samson.m.njuguna@gmail.com>
2024-08-23 19:34:16 +0300

    install a modal dependency for the video interview

--- interview-app/pages/VideoTest.tsx
+++ interview-app/pages/VideoTest.tsx
@@ -3,18 +3,15 @@ import VideoTestBtn from '../components/VideoPreviewTest';
 import "react-responsive-modal/styles.css";
 import { createRoot } from 'react-dom/client';
 import Styles from '../styles/Audio.module.css';
-import Webcam from 'react-webcam';
-import { BrowserRouter } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-const token = Array.from({length: 32}, () => Math.random().toString(36)[2]).join('');
+const token = Array.from({ length: 32 }, () => Math.random().toString(36)[2]).join('');
 console.log(token);
 const VideoTest = () => {
-    
+
     const [displayAudio, setDisplayAudio] = useState(false);
-  
-     const baseUrl = "http://localhost:3001/interview-app/interview";
-   // const token ="4a979b61bfc749a7bb5a65eafc2aff65"
-   //const token ="a7873dbcc4b2432daf47e63440316d68"
+
+    const baseUrl = "http://localhost:3001/interview-app/interview";
+    // const token ="4a979b61bfc749a7bb5a65eafc2aff65"
+    //const token ="a7873dbcc4b2432daf47e63440316d68"
     const interviewType = "reactjs01_nodejs01"; // You might want to get this dynamically as well if neede
 
     // Construct the URL with the dynamic token
@@ -28,12 +25,12 @@ const VideoTest = () => {
 
         const GiveAudio = () => {
             if (TestAudioSectionPreview) {
-               
+
                 const root = createRoot(TestAudioSectionPreview);
                 root.render(
-                
-< VideoTestBtn  />
-                        );
+
+                    < VideoTestBtn />
+                );
             }
         };
 
@@ -51,13 +48,13 @@ const VideoTest = () => {
     }, []); // Empty dependency array means this effect runs once when the component mounts
 
     return (
-        
+
         <>
-           
+
             <div id='TestAudioSection' className={Styles.audioSection}>
                 <h1>Hi</h1>
                 <h1>Test your audio before you start your interview</h1>
-                
+
                 <button id='btn' className={Styles.startRecording}>Before you start Test your Video</button>
 
 
